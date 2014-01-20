0660ce0a0cec47cf5d7fc1a659f32009a7fca4ae
thibaultCha <thibaultcha@me.com>
2014-01-20 23:35:43 +0100

    reverse passing tests

--- test/array.js
+++ test/array.js
@@ -19,7 +19,7 @@ describe('Array', function () {
 
 	describe('maxContiguousSum', function () {
 		var arr1 = [3, -4, 8, 7, -10, 19, -3]
-		var arr2 = [2, -3, -3, 9, -29, 8, -9]
+		, arr2   = [2, -3, -3, 9, -29, 8, -9]
 		it('should return the max contiguous sum', function () {
 			array.maxContiguousSum(arr1).should.equal(24)
 			array.maxContiguousSum(arr2).should.equal(9)
@@ -34,7 +34,12 @@ describe('Array', function () {
 	})
 
 	describe('reverse', function () {
-
+		var arr1 = "hello"
+		, arr2   = "foobar"
+		it('should reverse an array', function () {
+			array.reverse(arr1.split('')).should.eql("olleh".split(''))
+			array.reverse(arr2.split('')).should.eql("raboof".split(''))
+		})
 	})
 
 	describe('maxIntNotIn', function () {
