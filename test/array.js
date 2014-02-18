86253c08e233981f06da2ad414eaac20aa1e2f4a
thibaultCha <thibaultcha@me.com>
2014-02-18 20:12:22 +0100

    Added pickPeacks() and misc.js for swap()

--- test/array.js
+++ test/array.js
@@ -57,4 +57,35 @@ describe('Array', function () {
 		})
 	})
 
+	describe('pickPeaks', function() {
+		it('sould support finding peaks', function() {
+			array.pickPeaks([1,2,3,6,4,1,2,3,2,1]).should.eql({"pos":[3,7],"peaks":[6,3]})
+			array.pickPeaks([1,2,5,4,3,2,3,6,4,1,2,3,3,4,5,3,2,1,2,3,5,5,4,3]).should.eql({"pos":[2,7,14,20],"peaks":[5,6,5,5]})
+		})
+		it('should support finding peaks, but should ignore peaks on the edge of the array', function() {
+			array.pickPeaks([3,2,3,6,4,1,2,3,2,1,2,3]).should.eql( {"pos":[3,7],"peaks":[6,3]})
+		})
+		it('should support finding peaks; if the peak is a plateau, it should only return the position of the first element of the plateau', function() {
+			array.pickPeaks([3,2,3,6,4,1,2,3,2,1,2,2,2,1]).should.eql({"pos":[3,7,10],"peaks":[6,3,2]})
+		})
+		it('should support finding peaks; if the peak is a plateau, it should only return the position of the first element of the plateau', function() {
+			array.pickPeaks([2,1,3,1,2,2,2,2,1]).should.eql({"pos":[2,4],"peaks":[3,2]})
+		})
+		it('should support finding peaks, but should ignore peaks on the edge of the array', function() {
+			array.pickPeaks([2,1,3,1,2,2,2,2]).should.eql({"pos":[2],"peaks":[3]})
+		})
+		it('should support finding peaks, but should ignore peaks on the edge of the array', function() {
+			array.pickPeaks([2,1,3,2,2,2,2,5,6]).should.eql({"pos":[2],"peaks":[3]})
+		})
+		it('should support finding peaks, despite the plateau', function() {
+			array.pickPeaks([2,1,3,2,2,2,2,1]).should.eql({"pos":[2],"peaks":[3]})
+		})
+		it('should return an object with empty arrays if the input is an empty array', function() {
+			array.pickPeaks([]).should.eql({"pos":[],"peaks":[]})
+		})
+		it('should return an object with empty arrays if the input does not contain any peak', function() {
+			array.pickPeaks([1,1,1,1]).should.eql({"pos":[],"peaks":[]})
+		})
+	})
+
 })
