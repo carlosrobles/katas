21436bf19bc9cd9a7cab70f92de298a85611c4e9
thibaultCha <thibaultcha@me.com>
2014-02-22 21:06:34 +0100

    Add findFirst

--- test/misc.js
+++ test/misc.js
@@ -3,14 +3,42 @@ var should = require('should')
 
 var misc = require('../lib/misc')
 
-describe('Misc', function () {
+describe('Misc', function() {
 
-    describe('swap', function () {
-        it('should swap 2 values', function () {
+    describe('swap', function() {
+        it('should swap 2 values', function() {
             misc.swap([1,2]).should.eql([2,1])
             misc.swap([34,193]).should.eql([193,34])
             misc.swap([-12,8]).should.eql([8,-12])
             misc.swap([-6,-3]).should.eql([-3,-6])
+        })
+    })
+
+    describe('findFirst', function () {
+        // Paris -> Madrid; Madrid -> Lisbon; Lisbon -> New York; New York -> San Francisco
+        var tickets = [
+            {
+                "origin": "Madrid",
+                "destination": "Lisbon"
+            },
+            {
+                "origin": "Paris",
+                "destination": "Madrid"
+            },
+            {
+                "origin": "Lisbon",
+                "destination": "New York"
+            },
+            {
+                "origin": "New York",
+                "destination": "San Francisco"
+            }
+        ]
+        it('should find the first ticket of a trip', function() {
+            misc.findFirst(tickets).should.equal("Paris")
+        })
+        it('should return -1 if no origin could be determined', function() {
+            misc.findFirst([{ origin: "Paris", destination: "Paris" }]).should.equal(-1)
         })
     })
 })
